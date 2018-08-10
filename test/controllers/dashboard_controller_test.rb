6da2f3c29b1e2e9f53e52824936ed1b00c56b5fc
Joshua <joshuaprpich@gmail.com>
2018-08-10 17:19:41 -0500

    delete minitest

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,9 +1,1 @@
 require 'test_helper'
-
-class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get dashboard_index_url
-  #   assert_response :success
-  # end
-
-end
