02f74f082460b2e28f06c5741d782e4ec633925f
Joshua <joshuaprpich@gmail.com>
2018-08-10 17:04:39 -0500

    add circleci folder

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 feature 'visiting the homepage' do
-  scenario 'the visitor sees the welcome text' do
+  scenario 'the visitor sees expensify general statistics' do
     visit root_path
     expect(page).to have_text("Últimos 6 Meses")
+    expect(page).to have_text("Por Categoría")
+    expect(page).to have_text("Por Día")
+    expect(page).to have_text("Acumulado")
   end
 end
