ba41653464bc3588210072f763afedfe93313c10
Joshua <joshuaprpich@gmail.com>
2018-08-11 10:26:52 -0500

    create expenses new modal

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,4 +2,7 @@ class ExpensesController < ApplicationController
   def index
     @tab = :expenses
   end
+  def new
+    @expense = Expense.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "dashboard#index"
 
-  resources :expenses, only: [:index]
+  resources :expenses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
