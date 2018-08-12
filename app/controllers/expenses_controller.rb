41da4e92c30d83899fcf4f7d6bcb3741992ac523
Joshua <joshuaprpich@gmail.com>
2018-08-12 09:10:21 -0500

    complete expenses controller actions, add create functionality with ajax

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,42 @@
 class ExpensesController < ApplicationController
+  before_action :fetch_expense, only: [:update, :edit, :destroy, :show]
   def index
     @tab = :expenses
   end
   def new
     @expense = Expense.new
   end
+  def create
+    @expense = Expense.create(expense_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @expense.update(expense_params)
+      flash[:success] = "Expense was updated correctly"
+      redirect_to @expense
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    flash[:error] = "Expense was deleted correctly"
+    redirect_to expenses_path
+  end
+
+  private
+    def expense_params
+      params.require(:expense).permit(:concept, :date, :amount, :transaction_type, :category)
+    end
+
+    def fetch_expense
+      @expense = Expense.find_by(id: params[:id])
+    end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   enum transaction_type: { purchase: 0, withdrawl: 1, transfer: 2, payment: 3 }
   enum category: { restaurants: 0, grocery: 1, car: 2, services: 3, home: 4, education: 5, fun: 6, travel: 7 }
 
